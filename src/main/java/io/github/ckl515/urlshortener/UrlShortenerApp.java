ae71fe9b44d28eca3b3a6bc32e9d739f636bdcc1
klc124 <klc124@ic.ac.uk>
2026-01-11 20:06:50 +0000

    [feat] add option to input without arguments in CLI to prevent program exiting and allowing the 'get' and 'list' arguments to function properly

--- src/main/java/io/github/ckl515/urlshortener/UrlShortenerApp.java
+++ src/main/java/io/github/ckl515/urlshortener/UrlShortenerApp.java
@@ -6,6 +6,8 @@ import io.github.ckl515.urlshortener.repository.InMemoryUrlRepository;
 import io.github.ckl515.urlshortener.repository.UrlRepository;
 import io.github.ckl515.urlshortener.service.UrlShortenerService;
 
+import java.util.Scanner;
+
 public class UrlShortenerApp {
     public static void main(String[] args) {
         UrlRepository repository = new InMemoryUrlRepository();
@@ -13,6 +15,38 @@ public class UrlShortenerApp {
         UrlShortenerService service = new UrlShortenerService(repository, codeGenerator);
         CommandHandler commandHandler = new CommandHandler(service);
 
-        commandHandler.handleCommand(args);
+        if (args.length == 0) {
+            // Interactive mode
+            runInteractiveMode(commandHandler);
+        } else {
+            // Single command mode
+            commandHandler.handleCommand(args);
+        }
+    }
+
+    private static void runInteractiveMode(CommandHandler commandHandler) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type 'exit' to quit");
+        System.out.println();
+
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine().trim();
+
+            if (input.equals("exit")) {
+                System.out.println("URL Shortener program exited");
+                break;
+            }
+
+            if (input.isEmpty()) {
+                continue;
+            }
+
+            String[] args = input.split("\\s+");
+            commandHandler.handleCommand(args);
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }
