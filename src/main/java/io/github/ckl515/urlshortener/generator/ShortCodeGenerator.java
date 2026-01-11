598237cf1641f2bc2cb37d5ebfcee31d4b0c590a
klc124 <klc124@ic.ac.uk>
2026-01-11 18:50:35 +0000

    [ref] use char[] instead of String for valid characters and build the array instead of hardcoding

--- src/main/java/io/github/ckl515/urlshortener/generator/ShortCodeGenerator.java
+++ src/main/java/io/github/ckl515/urlshortener/generator/ShortCodeGenerator.java
@@ -8,14 +8,29 @@ import java.util.Random;
 
 public class ShortCodeGenerator {
     private static final Logger logger = LoggerFactory.getLogger(ShortCodeGenerator.class);
-    private static final String CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final char[] CHARS;
     private static final int CODE_LENGTH = 6;
     private final Random random = new SecureRandom();
+
+    // set up all valid alphabets and digits
+    static {
+        CHARS = new char[62];
+        int pos = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            CHARS[pos++] = c;
+        }
+        for (char c = 'A'; c <= 'Z'; c++) {
+            CHARS[pos++] = c;
+        }
+        for (char c = '0'; c <= '9'; c++) {
+            CHARS[pos++] = c;
+        }
+    }
 
     public String generate() {
         StringBuilder code = new StringBuilder();
         for (int i = 0; i < CODE_LENGTH; i++) {
-            code.append(CHARS.charAt(random.nextInt(CHARS.length())));
+            code.append(CHARS[random.nextInt(CHARS.length)]);
         }
         logger.debug("Generated short code: {}", code);
         return code.toString();
